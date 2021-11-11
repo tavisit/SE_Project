f12dc31ae64aa63728052657532952479114a5d0
Virghileanu Teodor <teo.virghi@yahoo.ro>
2021-11-11 21:50:49 +0200

    include blue input and button

---
+++ components/Alt/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <BaseButton
+    class="focus:(outline-none ring ring-blue-300) text-blue-600 bg-blue-50 border-blue-200 hover:bg-blue-100 active:bg-blue-200"
+  >
+    <slot />
+  </BaseButton>
+</template>
---
+++ components/Alt/Input.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export default defineComponent({
+
+});
+</script>
+
+<template>
+  <BaseInput
+    class="text-blue-700 focus:(outline-none ring ring-blue-100) bg-white border-blue-200 hover:bg-blue-50 active:bg-blue-100"
+  >
+    <!-- Take all scoped slots, and pass them to the BaseInput -->
+    <template
+      v-for="(_, slot) in $slots"
+      v-slot:[slot]="scope"
+    >
+      <slot :name="slot" v-bind="scope || {}" />
+    </template>
+  </BaseInput>
+</template>
