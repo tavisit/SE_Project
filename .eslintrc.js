9025b0fcb4268ae4f714768589d751c5712fd4e0
GaussianWonder <teo.virghi@yahoo.ro>
2022-01-26 17:47:39 +0200

    remove unnecessary eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,28 +25,3 @@ module.exports = {
     semi: [2, "always"],
   },
 };
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//   },
-//   extends: [
-//     'plugin:vue/essential',
-//     '@nuxtjs',
-//     'plugin:prettier/recommended',
-//   ],
-//   parserOptions: {
-//     ecmaVersion: 12,
-//     parser: '@typescript-eslint/parser',
-//     sourceType: 'module',
-//   },
-//   plugins: ['vue'],
-// rules: {
-//   'vue/multi-word-component-names': 'warn',
-//   'no-unused-vars': 'warn',
-//   'space-in-parens': 'off',
-//   'computed-property-spacing': 'off',
-//   'max-len': 'warn',
-//   'semi': [2, 'always'],
-// },
-// }
