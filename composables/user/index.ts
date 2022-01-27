79347e26d8117c274ed15b07cbb3c553c39dc783
GaussianWonder <teo.virghi@yahoo.ro>
2022-01-27 15:31:11 +0200

    minor changes

--- composables/user/index.ts
+++ composables/user/index.ts
@@ -3,19 +3,20 @@ import { IUser, User } from './types';
 
 const cookieOptions: CookieOptions<IUser> = {
   sameSite: 'strict',
+  httpOnly: false,
   expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 365),
 };
 
 export const useAuth = () => {
   // mingle User objects, update cookies when needed
   const userRef = useState('user', () => {
-    const iuser = new User(useCookie<IUser>('user').value);
+    const iuser = new User(useCookie<IUser>('user', cookieOptions).value);
     // convert to pure object, see comments of User class
     return { ...iuser };
   });
 
   watch(userRef, (newUser) => {
-    useCookie<IUser>('user').value = newUser;
+    useCookie<IUser>('user', cookieOptions).value = newUser;
   });
 
   return userRef;
--- server/utils/firebase_logic/auth.ts
+++ server/utils/firebase_logic/auth.ts
@@ -5,23 +5,11 @@ import {
 } from 'firebase/auth';
 
 export const register = async (email: string, password: string) => {
-  try {
-    return await createUserWithEmailAndPassword(auth, email, password);
-  } catch (error: any) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage, error);
-    return null;
-  }
+  const user = await createUserWithEmailAndPassword(auth, email, password);
+  return user;
 };
 
 export const login = async (email: string, password: string) => {
-  try {
-    return await signInWithEmailAndPassword(auth, email, password);
-  } catch (error: any) {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode, errorMessage, error);
-    return null;
-  }
+  const user = await signInWithEmailAndPassword(auth, email, password);
+  return user;
 };
